520927cc1850784dd5c3a010e50d4202a611025e
Grzegorz Dziegielewski <jabbas@jabbas.eu>
2015-08-16 18:35:50 +0200

    added maintainer info

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
     name = "pynapi",
+    author = 'Grzegorz Dzięgielewski',
+    author_email = 'jabbas@jabbas.eu',
+    url = "https://github.com/jabbas/pynapi",
     version = '0.5',
     description = 'subtitles downloader',
     entry_points = { 'console_scripts': [ 'pynapi = pynapi.cmdline:cmdline' ] },
