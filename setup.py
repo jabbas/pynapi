4a21808fefeb4d13bf3693fb190fba471a1e8c0b
Grzegorz Dziegielewski <jabbas@jabbas.eu>
2015-08-21 10:17:40 +0200

    fixed missing stuff in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     author = 'Grzegorz Dzięgielewski',
     author_email = 'jabbas@jabbas.eu',
     url = "https://github.com/jabbas/pynapi",
-    version = '0.5.2',
+    license = 'GPLv3',
+    version = '0.5.3',
     description = 'subtitles downloader',
+    long_description = open('README.md').read(),
     entry_points = { 'console_scripts': [ 'pynapi = pynapi.cmdline:cmdline' ] },
     packages = find_packages()
 )
