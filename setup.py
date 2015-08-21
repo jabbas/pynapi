ed6b12ea4d5510eb128a642c603f2f7541075a52
Grzegorz Dziegielewski <jabbas@jabbas.eu>
2015-08-21 09:54:10 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = 'Grzegorz Dzięgielewski',
     author_email = 'jabbas@jabbas.eu',
     url = "https://github.com/jabbas/pynapi",
-    version = '0.5.1',
+    version = '0.5.2',
     description = 'subtitles downloader',
     entry_points = { 'console_scripts': [ 'pynapi = pynapi.cmdline:cmdline' ] },
     packages = find_packages()
